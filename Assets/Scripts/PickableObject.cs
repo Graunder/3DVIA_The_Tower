a3c37543e394aa065138b85a518e0f399ad66f12
Graunder <graunder@gmail.com>
2018-03-12 01:49:15 +0200

    Test_Carry
    Pievienots:
    Test_Carry scene
    PickableObject skripts
    
    Left click: pacelt objektu
    Right click: atlaist objektu

---
+++ Assets/Scripts/PickableObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour {
+
+	public Transform TransformCamera;
+	public LayerMask RayMask;
+
+	private RaycastHit hit;
+	private Transform currentTransform;
+	private float lenght;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			if(Physics.Raycast(TransformCamera.position, TransformCamera.forward, out hit, 3f, RayMask)){
+				if(hit.transform.tag == "PickableObject"){
+					SetNewTransform (hit.transform);
+				}
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.Mouse1)){
+			RemoveTransform ();
+		}
+
+		if(currentTransform){
+			MoveTransformAround ();
+		}
+	}
+
+	public void SetNewTransform(Transform newTransform){
+		if(currentTransform){
+			return;
+		}
+
+		currentTransform = newTransform;
+		lenght = Vector3.Distance (TransformCamera.position, newTransform.position);
+		currentTransform.GetComponent<Rigidbody> ().isKinematic = true;
+	}
+
+	private void MoveTransformAround(){
+		currentTransform.position = TransformCamera.position + TransformCamera.forward * lenght;
+	}
+
+	public void RemoveTransform(){
+		if(!currentTransform){
+			return;
+		}
+
+		currentTransform.GetComponent<Rigidbody> ().isKinematic = false;
+		currentTransform = null;
+	}
+}
