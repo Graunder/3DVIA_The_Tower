216f6d28bea6a38a051ba57376a71fca3027e7ff
Graunder <graunder@gmail.com>
2018-04-15 03:11:11 +0300

    Fireball
    Pievienots:
    Fireball detalas
    fireball prefab
    Jauns fps prefabs
    Iespeja saut fireball(Izmantojot "T")

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public Transform spawn;
+	private float bulletSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.T)){
+
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, spawn.position, spawn.rotation);
+			//clone.velocity = spawn.TransformDirection (Vector3.forward*20);
+			clone.AddForce(clone.transform.forward * 1000);
+
+		}
+	}
+}
