c38b95ae59dac82595dc6de6af0eb39acc40d1d6
Graunder <graunder@gmail.com>
2018-04-20 17:10:32 +0300

    FixFixFix
    Salabotas lietas un pievienota pedeja prasiba

---
+++ Assets/Scripts/TargetMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMove : MonoBehaviour {
+
+	public Transform[] path;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (FollowPath());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FollowPath(){
+		foreach (Transform waypoint in path) {
+			yield return StartCoroutine (Move (waypoint.position, speed));
+		}
+	}
+
+	IEnumerator Move(Vector3 destination, float speed){
+		while(transform.position != destination){
+			transform.position = Vector3.MoveTowards (transform.position, destination, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
+}
