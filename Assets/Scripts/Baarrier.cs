ad52480a177df36d6b24bf4f67acdd65ff843938
Graunder <graunder@gmail.com>
2018-04-20 20:17:50 +0300

    So Close
    Pievienots:
    Trukstosas texturas
    Paabeigti limeni
    Saavienojumi starp limeniem
    
    Trukst:
    Portal effect
    Instrukcijas
    Main Menu

---
+++ Assets/Scripts/Baarrier.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baarrier : MonoBehaviour {
+
+	public GameObject key;
+	public GameObject target;
+
+	void OnTriggerEnter(Collider col){
+		if(col.name == key.name){
+			Destroy (target);
+		}
+	}
+}
